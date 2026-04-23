363a3f2c85d010a3e3cbac153214ac8fa30c36c8
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-04-23 17:36:42 +0530

    Time: 3 ms (18.88%) | Memory: 21.9 MB (12.63%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -5,7 +5,7 @@ public:
         int total = n * (n + 1) / 2;
         int sum = 0;
         for(int i = 0; i < n; i++){
-            sum += nums[i];
+            sum = sum + nums[i];
         }
         return total - sum;
     }
