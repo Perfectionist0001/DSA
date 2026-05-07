efc238fbb875ecf633ca65c0b73beacc404d51e0
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-05-07 14:59:24 +0530

    Time: 0 ms (100.00%) | Memory: 15.6 MB (92.52%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        bool ok=next_permutation(nums.begin(),nums.end());
-        if(!ok)
-        {
-            sort(nums.begin(),nums.end());
-        }
+        next_permutation(nums.begin(),nums.end());
     }
 };
