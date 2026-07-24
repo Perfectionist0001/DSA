283c73c92e473735e63114c5ade28e632616a3be
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-07-24 14:54:32 +0530

    Time: 87 ms (5.11%), Space: 14.8 MB (99.74%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,19 +1,69 @@
+// class Solution {
+// public:
+//     vector<int> frequencySort(vector<int>& nums) {
+//         // unordered_map<int,int> freq;
+//         // for(int x : nums)
+//         // {
+//         //     freq[x]++;
+//         // }
+//         // sort(nums.begin(), nums.end(), [&](int a, int b)
+//         // {
+//         //     if(freq[a] == freq[b])
+//         //     {
+//         //         return a > b;
+//         //     }
+//         //     return freq[a] < freq[b];
+//         // });
+//         // return nums;
+//         int n = nums.size();
+//         int freq1 = 0;
+//         int freq2 = 0;
+//         for(int i = 0; i < n; i++){
+//             for(int j = i + 1; j < n; j++){
+//                 for(int k = 0; k < n; k++){
+//                     if(nums[k] == nums[i]){
+//                         freq1++;
+//                     }
+//                     if(nums[k] == nums[j]){
+//                         freq2++;
+//                     }
+//                     if (freq1 > freq2 || (freq1 == freq2 && nums[i] < nums[j])){
+//                         swap(nums[i] , nums[j]);
+//                     }
+//                 }
+//             }
+//         }
+//         return nums;
+//     }
+// };
 class Solution {
 public:
     vector<int> frequencySort(vector<int>& nums) {
-        unordered_map<int,int> freq;
-        for(int x : nums)
+        int n = nums.size();
+        for(int i = 0; i < n - 1; i++)
         {
-            freq[x]++;
-        }
-        sort(nums.begin(), nums.end(), [&](int a, int b)
-        {
-            if(freq[a] == freq[b])
+            for(int j = i + 1; j < n; j++)
             {
-                return a > b;
+                int freq1 = 0;
+                int freq2 = 0;
+                for(int k = 0; k < n; k++)
+                {
+                    if(nums[k] == nums[i])
+                    {
+                        freq1++;
+                    }
+
+                    if(nums[k] == nums[j])
+                    {
+                        freq2++;
+                    }
+                }
+                if(freq1 > freq2 || (freq1 == freq2 && nums[i] < nums[j]))
+                {
+                    swap(nums[i], nums[j]);
+                }
             }
-            return freq[a] < freq[b];
-        });
+        }
         return nums;
     }
 };
