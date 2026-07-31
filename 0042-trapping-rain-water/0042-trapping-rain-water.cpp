9095215c55fac52d8ad59f2eae69c277fb5a5319
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-07-31 14:41:14 +0530

    Time: 0 ms (100%), Space: 27.2 MB (34.83%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -18,29 +18,51 @@
 //     }
 // };
 
+// class Solution {
+// public:
+//     int trap(vector<int>& height) {
+//         int left = 0;
+//         int right = height.size() - 1;
+//         int leftMax = 0;
+//         int rightMax = 0;
+//         int water = 0;
+//         while (left <= right) {
+//             if (height[left] <= height[right]) {
+//                 if (height[left] >= leftMax)
+//                     leftMax = height[left];
+//                 else
+//                     water = water + leftMax - height[left];
+//                 left++;
+//             } else {
+//                 if (height[right] >= rightMax)
+//                     rightMax = height[right];
+//                 else
+//                     water = water + rightMax - height[right];
+//                 right--;
+//             }
+//         }
+//         return water;
+//     }
+// };
+
 class Solution {
-public:
+    public:
     int trap(vector<int>& height) {
-        int left = 0;
-        int right = height.size() - 1;
-        int leftMax = 0;
-        int rightMax = 0;
-        int water = 0;
-        while (left <= right) {
-            if (height[left] <= height[right]) {
-                if (height[left] >= leftMax)
-                    leftMax = height[left];
-                else
-                    water = water + leftMax - height[left];
-                left++;
-            } else {
-                if (height[right] >= rightMax)
-                    rightMax = height[right];
-                else
-                    water = water + rightMax - height[right];
-                right--;
-            }
+        int n = height.size();
+        int ans = 0;
+        vector<int>Lmax(n,0);
+        vector<int>Rmax(n,0);
+        Lmax[0] = height[0];
+        Rmax[n - 1] = height[n - 1];
+        for(int i = 1; i < n; i++){
+            Lmax[i] = max(Lmax[i - 1], height[i]);
         }
-        return water;
+        for(int i = n - 2; i >= 0; i--){
+            Rmax[i] = max(Rmax[i + 1],height[i]);
+        }
+        for(int i = 0; i < n; i++){
+            ans = (ans + min(Lmax[i],Rmax[i]) - height[i]);
+        }
+        return ans;
     }
 };
