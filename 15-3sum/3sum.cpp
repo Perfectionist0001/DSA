e16820888a330dfabca1ef3ebe12609cf6a0badc
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-08-10 01:52:58 +0530

    Time: 44 ms (74.73%) | Memory: 29 MB (73.55%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,22 +1,7 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        // set<vector<int>>s;
-        // int n = nums.size();
-        // for(int i = 0; i < n;i ++){
-        //     for(int j = i + 1; j < n; j++){
-        //         for(int k = j + 1; k < n; k++){
-        //             if(nums[i] + nums[j] + nums[k] == 0){
-        //             vector<int>temp = {nums[i] , nums[j] , nums[k]};
-        //             sort(temp.begin(),temp.end());
-        //             s.insert(temp);
-        //             }
-        //         }
-        //     }
-        // }
-        // vector<vector<int>> ans(s.begin(),s.end());
-        // return ans;
-        vector<vector<int>> ans;
+        vector<vector<int>> result;
         int n = nums.size();
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n - 2; i++) {
@@ -27,12 +12,11 @@ public:
             while(left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
                 if(sum == 0) {
-                    ans.push_back({nums[i], nums[left], nums[right]});
+                    result.push_back({nums[i], nums[left], nums[right]});
                     while(left < right && nums[left] == nums[left + 1])
                         left++;
                     while(left < right && nums[right] == nums[right - 1])
                         right--;
-
                     left++;
                     right--;
                 }
@@ -44,6 +28,6 @@ public:
                 }
             }
         }
-        return ans;
+        return result;
     }
 };
