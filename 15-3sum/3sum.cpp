6a20c840964617d39f2b663728dd35310c15d251
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-08-10 17:16:34 +0530

    Time: 52 ms (34.51%) | Memory: 28.9 MB (92.72%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -5,8 +5,7 @@ public:
         int n = nums.size();
         sort(nums.begin(), nums.end());
         for(int i = 0; i < n - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
+            if(i > 0 && nums[i] == nums[i - 1])continue;
             int left = i + 1;
             int right = n - 1;
             while(left < right) {
@@ -14,9 +13,9 @@ public:
                 if(sum == 0) {
                     result.push_back({nums[i], nums[left], nums[right]});
                     while(left < right && nums[left] == nums[left + 1])
-                        left++;
+                    left++;
                     while(left < right && nums[right] == nums[right - 1])
-                        right--;
+                    right--;
                     left++;
                     right--;
                 }
