ce5ebb3af221bcb477ae70787bda47513fd1afe9
Nikhil Singh <149193023+Perfectionist0001@users.noreply.github.com>
2026-08-20 12:30:10 +0530

    Time: 48 ms (37.76%), Space: 84.8 MB (63.3%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -3,16 +3,14 @@ public:
     int totalFruit(vector<int>& fruits) {
         int n = fruits.size();
         int low = 0;
-        int high = 0;
         int result = 0;
         unordered_map<int,int> mp;
-        for(high = 0; high < n; high++) {
+        for(int high = 0; high < n; high++) {
             mp[fruits[high]]++;
             while(mp.size() > 2) {
                 mp[fruits[low]]--;
-                if(mp[fruits[low]] == 0) {
+                if(mp[fruits[low]] == 0)
                     mp.erase(fruits[low]);
-                }
                 low++;
             }
             int length = high - low + 1;
